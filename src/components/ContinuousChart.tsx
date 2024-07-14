5f85ff49900672e331eb11bc61f81955de3cffdd
Winston Edwin Chiong <winstonchiong@berkeley.edu>
2024-07-14 03:43:52 -0700

    add new chart; still doesn't work and always re-renders

--- src/components/ContinuousChart.tsx
+++ src/components/ContinuousChart.tsx
@@ -11,21 +11,68 @@ import { Data } from "../interfaces/interfaces";
 
 Chart.register(CategoryScale, LinearScale, PointElement, LineElement, Filler);
 
-export default function ContinuousChart({ data }: { data: Data }) {
+export default function ContinuousChart({ data }: { data: Data[] }) {
+  console.log("rendering continuous chart");
   const chartData = {
-    labels: data.x,
     datasets: [
       {
         label: "Probability Density Function",
-        data: data.y,
+        data: data[0].x.map((x, i) => ({ x: x, y: data[0].y[i] })),
         fill: true,
         backgroundColor: "rgba(249, 115, 22, 0.6)",
-        borderColor: "rgb(249 115 22)",
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        type: "line",
+        label: "Probability Density Function",
+        data: data[1].x.map((x, i) => ({ x: x, y: data[1].y[i] })),
+        fill: true,
+        backgroundColor: "rgba(109, 115, 22, 0.6)",
+        tension: 0.1,
+        pointRadius: 0,
+      },
+      {
+        type: "bar",
+        label: "Probability Density Function",
+        data: data[2].x.map((x, i) => ({ x: x, y: data[2].y[i] })),
+        fill: true,
+        backgroundColor: "rgba(9, 115, 22, 0.6)",
         tension: 0.1,
         pointRadius: 0,
       },
     ],
   };
 
-  return <Line data={chartData} />;
+  const options = {
+    scales: {
+      x: {
+        type: "linear",
+        title: {
+          display: true,
+          text: "Values",
+        },
+      },
+    },
+  };
+  // const options = {
+  //   scales: {
+  //     x: {
+  //       type: "linear",
+  //       title: {
+  //         display: true,
+  //         text: "Values",
+  //       },
+  //     },
+  //     y: {
+  //       beginAtZero: true,
+  //       title: {
+  //         display: true,
+  //         text: "Probability",
+  //       },
+  //     },
+  //   },
+  // };
+
+  return <Line data={chartData} options={options}/>;
 }
